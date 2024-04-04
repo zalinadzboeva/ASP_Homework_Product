29daf73c285eb78a7a7b429ad6aba84d593f2362
zalina.dzboeva@outlook.com <zalina.dzboeva@outlook.com>
2024-04-04 17:41:21 +0300

    Dzboeva_Zalina_3_1

---
+++ ASP_Homework_Product/Controllers/CatalogController1.cs
@@ -0,0 +1,13 @@
+﻿using ASP_Homework_Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class CatalogController1 : Controller
+    {
+        public string Index()
+        {
+            return "ghbdtn";
+        }
+    }
+}
